50995742e8ad9a95c3eac97f10900d6974823707
olibraun <oliverbraun89@gmail.com>
2017-09-26 20:07:30 +0200

    Initiale Befüllung Array

--- rullo/sketch.js
+++ rullo/sketch.js
@@ -9,22 +9,36 @@ function make2DArray(cols,rows){
 }
 
 var n;
+var myNumbers;
+var offset;
 
 function setup() {
   createCanvas(500,700);
-  n = new num(120,120,1);
+  offset = 100;
+  myNumbers = make2DArray(5,5);
+  for(var i=0; i < 5; i++){
+    for(var j=0; j < 5; j++){
+      myNumbers[i][j] = new num(i*70 + offset,j*70 + offset,floor(random(1,10)));
+    }
+  }
 }
 
 function draw() {
   background(51);
-  n.show();
+  for(var i=0; i < 5; i++){
+    for(var j=0; j < 5; j++){
+      myNumbers[i][j].show();
+    }
+  }
 }
 
 function mouseClicked(){
-  console.log(mouseX);
-  console.log(mouseY);
-  console.log(n.pos);
-  if(n.hits(mouseX,mouseY)){
-    n.switchActive();
+  console.log(floor(random(1,10)));
+  for(var i=0; i < 5; i++){
+    for(var j=0; j < 5; j++){
+      if(myNumbers[i][j].hits(mouseX,mouseY)){
+        myNumbers[i][j].switchActive();
+      }
+    }
   }
 }
