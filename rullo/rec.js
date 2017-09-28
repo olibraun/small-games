15578f3418857d7b855bd749ef3a704882e6a084
olibraun <oliverbraun89@gmail.com>
2017-09-28 22:30:39 +0200

    Color functionality for rectangles.

--- rullo/rec.js
+++ rullo/rec.js
@@ -1,19 +1,34 @@
 rec = function(x,y){
   this.pos = createVector(x,y);
   this.value = 0;
+  this.targetReached = false;
 
   this.setValue = function(n){
     this.value = n;
   }
 
   this.show = function(){
-    stroke(179, 179, 204);
+    if(!this.targetReached){
+      //Grau
+      stroke(179, 179, 204);
+    }else{
+      //Gelb
+      stroke(244, 235, 66);
+    }
+
     strokeWeight(3);
     noFill();
     rectMode(CENTER);
     rect(this.pos.x,this.pos.y,50,50);
 
-    fill(255);
+    if(!this.targetReached){
+      //Schrift weiß
+      fill(255);
+    }else{
+      //Schrift gelb
+      fill(244, 235, 66);
+    }
+
     noStroke();
     textAlign(CENTER,CENTER);
     textSize(20);
