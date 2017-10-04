e8f0a0d1efc2ba9e30974a363ee25c404e5c094d
olibraun <oliverbraun89@gmail.com>
2017-10-04 21:40:07 +0200

    First draft info text

--- rullo/infoscreen.js
+++ rullo/infoscreen.js
@@ -1,10 +1,20 @@
 infoScreen = function(){
+  let txt = "";
+
+  txt = txt + "RULLO CLONE" +"\n";
+  txt = txt +"\n";
+  txt = txt + "Ziel des Spiels ist [TO DO]"+"\n";
+  txt = txt +"\n";
+  txt = txt +"\n";
+  txt = txt +"\n";
+  txt = txt + "Bitte klicken Sie, um weiter zu spielen.";
+
   this.show = function(){
     fill(98, 155, 247);
     noStroke();
     textAlign(CENTER,CENTER);
-    textSize(35);
+    textSize(20);
     textStyle(NORMAL);
-    text("Infotext",width/2,height/2);
+    text(txt,width/2,height/2);
   }
 }
