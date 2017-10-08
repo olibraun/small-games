f6146871eff0f265a6da5fe10358b66f3a8a2bfd
olibraun <oliverbraun89@gmail.com>
2017-10-08 12:36:12 +0200

    Add proper info message

--- rullo/infoscreen.js
+++ rullo/infoscreen.js
@@ -3,11 +3,23 @@ infoScreen = function(){
 
   txt = txt + "RULLO CLONE" +"\n";
   txt = txt +"\n";
-  txt = txt + "Ziel des Spiels ist [TO DO]"+"\n";
   txt = txt +"\n";
+  txt = txt + "Deactivate circles by clicking in order to"+"\n";
+  txt = txt + "achieve the target numbers in the squares." + "\n";
+  txt = txt + "" + "\n";
+  txt = txt + "A long click on a circle will lock it in its state." + "\n";
+  txt = txt + "That way it can no longer be (de-)activated." + "\n";
+  txt = txt + "" + "\n";
+  txt = txt + "A finished row/column has its square yellow." + "\n";
+  txt = txt + "A click on a finished square will lock or" + "\n";
+  txt = txt + "unlock the entire row/column." + "\n";
+  txt = txt + "" + "\n";
+  txt = txt + "Clicking on an unfinished square will" + "\n";
+  txt = txt + "reveal the sum of currently active" + "\n";
+  txt = txt + "circles in the respective row/column." + "\n";
+  txt = txt + "" + "\n";
   txt = txt +"\n";
-  txt = txt +"\n";
-  txt = txt + "Bitte klicken Sie, um weiter zu spielen.";
+  txt = txt + "Please click to continue.";
 
   this.show = function(){
     fill(98, 155, 247);
